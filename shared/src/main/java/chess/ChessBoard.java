c7dd2f41139d50470404f9953f05ae5e15fccaaa
Jack Hillman <jack.t.hillman@gmail.com>
2026-05-11 11:46:08 -0600

    new method (p1) deepCopy implementation for ChessBoard

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -82,6 +82,21 @@ public class ChessBoard {
         addPiece(new ChessPosition(7,8), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.PAWN));
     }
 
+    public ChessBoard DeepCopy(){
+        ChessBoard deepCopy = new ChessBoard();
+        for (int i = 1; i <= 8; i++) {
+            for (int j = 1; j < 8; j++) {
+                ChessPiece currentPiece = squares[i][j];
+                if (currentPiece != null) {
+                    ChessPiece.PieceType pieceType = currentPiece.getPieceType();
+                    ChessGame.TeamColor pieceColor = currentPiece.getTeamColor();
+                    deepCopy.addPiece(new ChessPosition(i,j), new ChessPiece(pieceColor, pieceType));
+                }
+            }
+        }
+        return deepCopy;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) {
