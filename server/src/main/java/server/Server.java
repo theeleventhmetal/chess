c2ff2daf88147fc0f68e5842aa0c50e3ae548766
Jack Hillman <jack.t.hillman@gmail.com>
2026-05-18 09:35:44 -0600

    (p3) updated README, completed getting started instructions

---
+++ server/src/main/java/server/Server.java
@@ -0,0 +1,22 @@
+package server;
+
+import io.javalin.*;
+
+public class Server {
+
+    private final Javalin javalin;
+
+    public Server() {
+        javalin = Javalin.create(config -> config.staticFiles.add("web"));
+
+    }
+
+    public int run(int desiredPort) {
+        javalin.start(desiredPort);
+        return javalin.port();
+    }
+
+    public void stop() {
+        javalin.stop();
+    }
+}
